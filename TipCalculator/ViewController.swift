8bb45c98ad9b8a47ccf30d49ba8c39b6ab771672
user145843 <user145843@74-80-245-25.ic2net.net>
2019-04-06 16:34:31 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by user145843 on 4/6/19.
+//  Copyright © 2019 T3Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
